0134d7ecb1b6fd1375ed238db94cd7507c492f38
Thierry0303 <thierry.amiot81@gmail.com>
2026-03-21 12:42:05 +0000

    Create enrich.py module for data enrichment

--- london_school_model/enrich.py
+++ london_school_model/enrich.py
@@ -1,30 +1,10 @@
-def enrich_data(data):
-    """
-    Enrich the provided data with additional information.
+"""Data enrichment module for adding features to school data"""
 
-    Parameters:
-    data (dict): Input data to be enriched.
-
-    Returns:
-    dict: Enriched data.
-    """
-    # Example enrichment logic
-    enriched_data = {"original_data": data}
-    enriched_data["additional_info"] = "Some additional information"
-    return enriched_data
+def enrich_with_imd(schools_df, imd_df):
+    """Enrich school data with IMD metrics"""
+    pass
 
 
-def improve_quality(data):
-    """
-    Improve the quality of the provided data.
-
-    Parameters:
-    data (dict): Input data to be improved.
-
-    Returns:
-    dict: Improved data.
-    """
-    # Example quality improvement logic
-    improved_data = {"original_data": data}
-    improved_data["quality_check"] = True
-    return improved_data
+def enrich_with_crime(schools_df, crime_df):
+    """Enrich school data with crime statistics"""
+    pass
